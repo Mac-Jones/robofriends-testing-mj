eb018cc5b97ad51402afe15a3e80dcc814c42136
macjonessoriano <sorianomacjones@gmail.com>
2023-04-02 23:26:45 +0800

    april 2 2023

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -3,5 +3,7 @@ import { shallow } from 'enzyme';
 import Card from './Card';
 
 it('renders without crashing', () => {
-  expect(shallow(<Card/>)).toMatchSnapshot();
+	expect(shallow(<Card />)).toMatchSnapshot();
+  // expect(shallow(<Card />).length).toEqual(1)
 });
+
--- src/components/CounterButton.test.js
+++ src/components/CounterButton.test.js
@@ -3,15 +3,21 @@ import { shallow } from 'enzyme';
 import CounterButton from './CounterButton';
 
 it('renders without crashing', () => {
-  expect(shallow(<CounterButton />)).toMatchSnapshot();
+  const mockColor = 'red'
+  expect(shallow(<CounterButton color={mockColor} />)).toMatchSnapshot();
 });
 
+// #counter or [id='counter']
 it('correnctly increments the counter', () => {
-  const wrapper = shallow(<CounterButton/>)
+  const mockColor = 'red'
+  const wrapper = shallow(<CounterButton color={mockColor}/>)
   expect(wrapper).toMatchSnapshot();
-  wrapper.find('[id="counter"]').simulate('click');
-  expect((wrapper.state())).toEqual({count: 1})
-  wrapper.find('[id="counter"]').simulate('click');
-  wrapper.find('[id="counter"]').simulate('click');
+  wrapper.find('#counter').simulate('click');
+  wrapper.find('#counter').simulate('click');
+  expect((wrapper.state())).toEqual({count: 2})
+  wrapper.find('#counter').simulate('click');
   expect((wrapper.state())).toEqual({count: 3})
+  wrapper.find('#counter').simulate('keypress');
+  expect((wrapper.state())).toEqual({count: 3})
+  expect(wrapper.props().color).toEqual('red')
 });
